c4de34bcd622ef1d8a646dd08c31c6fd79ba2913
guztorti <guztorti@gmail.com>
2022-07-06 13:57:35 -0300

    Spring biblioteca

---
+++ src/main/java/com/egg/biblioteca/entidades/Libro.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.egg.biblioteca.entidades;
+
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+/**
+ *
+ * @author Gustavo Torti
+ */
+@Entity
+public class Libro {
+    @Id
+    private Long ISBN;
+    private String titulo;
+    private Integer ejemplares;
+    @Temporal(TemporalType.DATE)
+    private Date fechaAlta;
+}
